b0bc5cf3c7d5b545476eb8ccac8b740b32f27637
Halil Yüksel <haloyuk.02@gmail.com>
2024-05-07 18:35:46 +0300

    Build URL Update
    Build URL Update

--- frontend/src/Component/MessageContainer/Message.jsx
+++ frontend/src/Component/MessageContainer/Message.jsx
@@ -11,7 +11,7 @@ const Message = ({ message }) => {
     const fromMe =
         (message.senderId || message.sender || message.userId) === authUser._id;
     const chatClassName = fromMe ? "messagesend1" : "receivedmessage1";
-    const fileUrl = `http://localhost:5000/${message.filePath}`;
+    const fileUrl = `https://chatcrypt.onrender.com/${message.filePath}`;
 
     const senderName = fromMe
         ? "You"
